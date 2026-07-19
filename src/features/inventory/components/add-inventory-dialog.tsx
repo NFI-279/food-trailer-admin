9d9e2fb282d092d4988a127555c4fdb2afdfa29e
Niculae Filip Ioan <filip_ioan.niculae@stud.fils.upb.ro>
2026-07-19 15:30:06 +0300

    Localize Add Inventory Dialog text

--- src/features/inventory/components/add-inventory-dialog.tsx
+++ src/features/inventory/components/add-inventory-dialog.tsx
@@ -13,10 +13,12 @@ import {
 import { InventoryForm } from "./inventory-form";
 import { useCreateInventoryItem } from "../hooks";
 import { InventoryItemInput } from "../types";
+import { useLanguage } from "@/providers/LanguageProvider";
 
 export function AddInventoryDialog() {
   const [open, setOpen] = useState(false);
   const createMutation = useCreateInventoryItem();
+  const { t } = useLanguage();
 
   const onSubmit = (data: InventoryItemInput) => {
     createMutation.mutate(data, {
@@ -27,13 +29,13 @@ export function AddInventoryDialog() {
   return (
     <>
       <Button onClick={() => setOpen(true)}>
-        <Plus className="mr-2 h-4 w-4" /> Add Item
+        <Plus className="mr-2 h-4 w-4" /> {t.inventory.btnAdd}
       </Button>
 
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>Add Inventory Item</DialogTitle>
+            <DialogTitle>{t.inventoryForm.addTitle}</DialogTitle>
           </DialogHeader>
           <InventoryForm onSubmit={onSubmit} isSubmitting={createMutation.isPending} />
         </DialogContent>
